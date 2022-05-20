c917400f37cf0fdb81d55e564781d538975e3472
Adham Salama <adhamsalama@zohomail.com>
2022-05-20 23:34:31 +0200

    Update example

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 # Example of using APF
 
-from apf import APF, Request
+from apf import APF, Request, Response, JSONResponse
 
 app = APF()
 
 
 @app.get("/hello")
 def hello(request: Request):
-    request.json_response(200, {"hello": "world"})
+    return JSONResponse(message={"hello": "world"})
+
+
+@app.get("/hi")
+def hi(request: Request):
+    return Response(
+        code=200,
+        message="<html><body><h1>Hello</h1></body></html>",
+        content_type="text/html; charset=UTF-8",
+    )
 
 
 app.run("localhost", 8080, multithreading=True)
