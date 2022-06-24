875f699d35202245b2f5e89b21ee9c9b148add21
Adham Salama <adhamsalama@zohomail.com>
2022-06-24 18:59:14 +0200

    Adjust ViewFunction Callable parameter type to Request

--- custom_types.py
+++ custom_types.py
@@ -1,11 +1,16 @@
-from typing import Callable, TypedDict, Any
+from __future__ import annotations
+from typing import Callable, TypedDict, TYPE_CHECKING
 
-# from request import Request
+if TYPE_CHECKING:
+    from request import Request
 from response import Response
 
 # ? It should take Request as a parameter but it results in circular import issue,
 # ? however, APF expects a function that takes Request as a parameter so type hinting works
-ViewFunction = Callable[[Any], Response]
+# ? Extra note: the the solution which uses TYPE_CHECKING doesn't work because in this case
+# ? ViewFunction = Callable[[Request], Response] is not a type hint, but an assignment.
+# ? However, I could just change Request to "Request" and it would work.
+ViewFunction = Callable[["Request"], Response]
 
 
 class RouteHandler(TypedDict):
