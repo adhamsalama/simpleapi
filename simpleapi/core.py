abdd31c1b22f08a2e355553e1565d6e751917897
Adham Salama <adham.salama@wuilt.com>
2022-08-19 06:29:24 +0200

    Refactor handling adding view functions to it's own class

---
+++ simpleapi/core.py
@@ -0,0 +1,54 @@
+
+
+from typing import Any
+
+from .custom_types import RouteHandler, ViewFunction
+
+
+class API:
+    """
+    Core API
+    
+    Exposes HTTP methods to add routing and a method for running the app.
+    """
+
+    def __init__(self) -> None:
+        self.handlers: list[RouteHandler] = []
+        self.body: dict[str, Any] = {}
+        self.extra: dict[Any, Any] = {}  # Extra dict for middleware to attach data to
+        self.query: dict[Any, list[str]] = {}
+        self.params: dict[str, str] = {}
+
+    def handle_request_decorator(self, path: str, method: str):
+        def decorator(handler: ViewFunction):
+
+            handler_dict: RouteHandler = {
+                "path": path,
+                "method": method,
+                "handler": handler,
+            }
+            self.handlers.append(handler_dict)
+            return handler
+
+        return decorator
+
+    def get(self, path: str):
+        return self.handle_request_decorator(path, "GET")
+
+    def post(self, path: str):
+        return self.handle_request_decorator(path, "POST")
+
+    def put(self, path: str):
+        return self.handle_request_decorator(path, "PUT")
+
+    def patch(self, path: str):
+        return self.handle_request_decorator(path, "PATCH")
+
+    def delete(self, path: str):
+        return self.handle_request_decorator(path, "DELETE")
+
+    def head(self, path: str):
+        return self.handle_request_decorator(path, "HEAD")
+
+    def options(self, path: str):
+        return self.handle_request_decorator(path, "OPTIONS")
--- simpleapi/simpleapi.py
+++ simpleapi/simpleapi.py
@@ -1,79 +1,26 @@
 # Author: Adham Salama
 
 import json
+from typing import Callable
 
+from .core import API
 from .custom_types import Environ
+from .handler import handle_request
 from .request import Request
 from .response import WSGIResponse
-
-from .custom_types import RouteHandler, ViewFunction, Environ
-from .request import Request
-from typing import Any, Callable
-from .handler import handle_request
-
-# def app(environ: Environ, start_response):
-
-#     request = Request(environ=environ)
-#     print(request.method,request.body, request.query, environ["SCRIPT_NAME"], environ["PATH_INFO"])
-#     # body = b"Hello world!\n"
-#     # status = "200 OK"
-#     # headers = [("Content-type", "text/plain")]
-#     # start_response(status, headers)
-#     # return [body]
-#     response = WSGIResponse(start_response,status="200 OK", headers=[("Content-type", "text/plain")])
-#     return response.send([b"Hello world!\n"])
-
+from .router import Router
 
 HTTP_METHODS = ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"]
 
 
-class SimpleAPI:
+class SimpleAPI(API):
     """
     SimpleAPI Class.
-
-    Exposes HTTP methods to add routing and a method for running the app.
     """
-
-    def __init__(self) -> None:
-        self.handlers: list[RouteHandler] = []
-        self.body: dict[str, Any] = {}
-        self.extra: dict[Any, Any] = {}  # Extra dict for middleware to attach data to
-        self.query: dict[Any, list[str]] = {}
-        self.params: dict[str, str] = {}
-
-    def handle_request_decorator(self, path: str, method: str):
-        def decorator(handler: ViewFunction):
-
-            handler_dict: RouteHandler = {
-                "path": path,
-                "method": method,
-                "handler": handler,
-            }
-            self.handlers.append(handler_dict)
-            return handler
-
-        return decorator
-
-    def get(self, path: str):
-        return self.handle_request_decorator(path, "GET")
-
-    def post(self, path: str):
-        return self.handle_request_decorator(path, "POST")
-
-    def put(self, path: str):
-        return self.handle_request_decorator(path, "PUT")
-
-    def patch(self, path: str):
-        return self.handle_request_decorator(path, "PATCH")
-
-    def delete(self, path: str):
-        return self.handle_request_decorator(path, "DELETE")
-
-    def head(self, path: str):
-        return self.handle_request_decorator(path, "HEAD")
-
-    def options(self, path: str):
-        return self.handle_request_decorator(path, "OPTIONS")
+    def add_router(self, prefix: str, router: Router):
+        for handler in router.handlers:
+            handler["path"] = prefix + handler["path"]
+            self.handlers.append(handler)
 
     def __call__(self, environ: Environ, start_response: Callable):
         request = Request(environ=environ)
