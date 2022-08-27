7f948fec780ce2c696f9e0095f589aef51c4320d
Adham Salama <adham.salama@wuilt.com>
2022-08-27 17:17:39 +0200

    Update docstring and add missing type annotations

--- simpleapi/request.py
+++ simpleapi/request.py
@@ -4,12 +4,26 @@ from . import utils
 
 
 class Request:
-    """HTTP Request"""
+    """
+    HTTP Request
+
+    Properties:
+    environ (The WSGI environ).
+    method (The request HTTP method)
+    path (The request path).
+    params (The route's dynamic parameters).
+    form (The request body's form data).
+    body (The request's JSON body).
+    query (The request's query parameters).
+    headers (The request's headers).
+    cookies (The requests's cookies).
+    extra (A dict to which middleware can add fields).
+    """
 
     def __init__(self, environ: utils.Environ) -> None:
         self.environ = environ
-        self.method = environ["REQUEST_METHOD"]
-        self.path = environ["PATH_INFO"]
+        self.method: str = environ["REQUEST_METHOD"]
+        self.path: str = environ["PATH_INFO"]
         self.extra: dict[str, Any] = {}
         self.params: dict[str, str] = {}
         self.form: dict[str, bytes]
